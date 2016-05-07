029dccd457d8b6bb8aa90a2b05d0bed504c7f2fb
flagctf <flagctf@users.noreply.github.com>
2016-05-07 14:00:19 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Welcome to Open!</title>
+    <link rel="stylesheet" href="/css/foundation.css">
+    </head>
+
+	<body>
+        <div class="row">
+            <div class="medium-9 medium-centered columns">
+                <h2 class="text-center">Welcome to Open!</h2>
+                <h5 class="page-intro">This is an important page, full of important information about the CTF.  Actually, it's not, but it could be.  The important thing is that the flag you're looking for is not on this page.  Maybe you should check github and see if you can figure out where the flag actually is.</h5>
+            </div>
+        </div>
+        <script src="/js/jquery.js"></script>
+        <script src="/js/foundation.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
+
